96704800d650f738046dad525c7d4cece48f5282
Manuel <manuel.penner@mein.gmx>
2024-10-29 21:04:27 +0100

    installed nodemon and added route to app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import './db/db.js';
 
 import categoryRouter from './routers/categoryRouter.js';
+import productsRouter from './routers/productsRouter.js';
 
 dotenv.config();
 const app = express();
@@ -14,6 +15,7 @@ app.get("/", (req, res) => {
 });
 
 app.use('/api/v1/categories', categoryRouter);
+app.use('/api/v1/products', productsRouter);
 
 app.listen(PORT, () => {
   console.log(`the server is running on http://localhost:${PORT}`);
