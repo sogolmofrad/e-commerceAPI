edbadae81e3dcab06b01b8031c41102c8596e363
Pureschko <inna.shekina@gmail.com>
2024-10-29 10:21:55 +0100

    categories

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-import express from "express";
+import express, { json } from 'express';
 import dotenv from "dotenv";
+import './db/db.js';
+
+import categoryRouter from './routers/categoryRouter.js';
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT;
+app.use(json());
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("the server is working!");
+});
+
+app.use('/api/v1/categories', categoryRouter);
+
+app.get('*', (req, res) => {
+  res.status(500).send('Server error!');
 });
 
 app.listen(PORT, () => {
