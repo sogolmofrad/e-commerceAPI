13765d633cd09a656b6f51c81ac3c017b1f6294c
Manuel <manuel.penner@mein.gmx>
2024-10-30 15:11:23 +0100

    routing fix

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import express, { json } from 'express';
 import dotenv from "dotenv";
 import './db/db.js';
 
-import categoryRouter from './routers/categoryRouter.js';
 import productsRouter from './routers/productsRouter.js';
 
 dotenv.config();
@@ -14,7 +13,6 @@ app.get("/", (req, res) => {
   res.send("the server is working!");
 });
 
-app.use('/api/v1/categories', categoryRouter);
 app.use('/api/v1/products', productsRouter);
 
 app.listen(PORT, () => {
