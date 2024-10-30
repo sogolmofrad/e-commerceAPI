2c2510ae70a3b7b89e763f5f7c4b00b61b8e9927
sogol mofrad <sogolmofrad@gmail.com>
2024-10-30 15:40:58 +0100

    add users

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import "./db/db.js";
+import userRouter from "./routers/userRouter.js";
 
 dotenv.config();
 const app = express();
+
+app.use(express.json());
+
 const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send("the server is working!");
 });
+
+app.use("/api/v1/users", userRouter);
 
 app.listen(PORT, () => {
   console.log(`the server is running on http://localhost:${PORT}`);
